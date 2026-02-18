d8fb8dcfb793974fe1ddefc22e3093c63024fcdd
faridaalakbarli <falakba@wgu.edu>
2026-02-17 22:18:46 -0800

    Modify Truck class

--- Truck.py
+++ Truck.py
@@ -1,11 +1,45 @@
+from datetime import time, timedelta
+
+
 class Truck:
-    max_size = 16
-    available = 16
-    package_list=[]
-    def __init__(self, id, status):
-        self.id = id
-        self.status = status
+
+
+    def __init__(self, truck_id, departure_time):
+        self.truck_id = truck_id
+        self.departure_time = departure_time
+        self.packages = []
+        self.current_location = "Hub"
+        self.mileage = 0.0
+        self.current_time = departure_time
+
+
+    def load_packages(self, package_id):
+        if self.packages < 16:
+            self.packages.append(package_id)
+        else:
+            print("Truck is full")
+
+
+    def travel_to(self, address, distance):
+        self.mileage += distance
+        travel_seconds = (distance / 18) * 3600
+        self.current_time += timedelta(seconds = travel_seconds)
+        self.current_location = address
+
+
+    def deliver_package(self, package_id, hashtable):
+        hashtable.update_status(package_id, "Delivered",
+                                self.current_time)
+
+        self.packages.remove(package_id)
+
+
+    def return_to_hub(self, distance):
+        self.travel_to("Hub", distance)
 
 
     def __str__(self):
-        return f"Truck {self.id} {self.status}"
+        return (f"Truck: {self.truck_id} | "
+                f"Mileage: {self.mileage} | "
+                f"Location: {self.current_location} | "
+                f"Time: {self.current_time}")
--- main.py
+++ main.py
@@ -1,7 +1,25 @@
 import DistanceTable
+import Truck
+import HashTable
+import Package
+import datetime
+import csv
 if __name__ == '__main__':
     distance_table = DistanceTable.DistanceTable('/Users/faridaalakbarli/Downloads/Distance_Table.csv')
     print(distance_table.get_distance(
         "International Peace Gardens\n 1060 Dalton Ave S",
         "Sugar House Park\n 1330 2100 S"))
+
+    hashtable = HashTable.HashTable()
+    with open('/Users/faridaalakbarli/Downloads/PackageFile.csv') as csvfile:
+        reader = csv.reader(csvfile)
+        for row in reader:
+            package = Package.Package(int(row[0]), row[1], row[2], row[4],
+                                      row[5], row[6], "at hub")
+            hashtable.insert(package)
+
+    hashtable.lookup(1)
+
+    truck1 = Truck.Truck(truck_id=1, departure_time=datetime.strptime("08:00", "%H:%M"))
+    truck1.load_packages()
     print(distance_table.addresses)
